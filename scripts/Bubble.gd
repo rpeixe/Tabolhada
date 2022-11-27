1f806c95cfa79139f57830671fc2277518a2b91c
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2022-11-26 23:47:21 -0300

    Alteração no tamanho da fonte, tipos de nós

--- scripts/Bubble.gd
+++ scripts/Bubble.gd
@@ -4,18 +4,39 @@ extends RigidBody2D
 var value
 var rng = RandomNumberGenerator.new()
 signal bubble_popped(bubble_value)
+var colors = [
+	Color.red,
+	Color.blue,
+	Color.yellow,
+	Color.orange,
+	Color.green,
+	Color.purple,
+	Color.teal,
+	Color.orangered,
+	Color.magenta,
+	Color.chartreuse,
+	Color.sandybrown,
+	Color.violet,
+]
 
-
-func _ready():
-	rng.randomize()
+func launch():
 	var x_velocity = rng.randf_range(-150, 150)
 	var y_velocity = rng.randf_range(-200, -300)
 	apply_central_impulse(Vector2(x_velocity, y_velocity))
 
+func set_random_color():
+	var color = colors[rng.randi_range(0, colors.size() - 1)]
+	$Sprite.modulate = color
+
+func _ready():
+	rng.randomize()
+	launch()
+	set_random_color()
+
 
 func init(num):
 	value = num
-	get_node("BubbleLabel").text = str(value)
+	$Label.text = str(value)
 
 
 func _on_Bubble_input_event(viewport, event, shape_idx):
