b26e1eff393bd5ab19b96a6901930705aa46c258
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2022-11-28 21:54:40 -0300

    Correção na velocidade

--- levels/main_game/question_board.gd
+++ levels/main_game/question_board.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-var bob_speed = 1000.0
+var bob_speed = 10.0
 
 func change_text(num_1, num_2):
 	$Multiplication.text = str(num_1) + "x" + str(num_2)
 
 func _physics_process(delta):
-	move_and_slide(Vector2.UP * cos($Timer.time_left) * bob_speed * delta)
+	move_and_slide(Vector2.UP * cos($Timer.time_left) * bob_speed)
